390980afc1bab08c5a4daa5871eed406637b7ecf
romalms10 <romalms10@gmail.com>
2018-06-11 22:08:26 +0000

    Write json to a file

---
+++ 0x01-warm_up/1-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' This module is used for getting information from an API '''
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    count = 0
+    true_list = []
+    json_dict = {}
+    response1 = requests.get(
+        'https://jsonplaceholder.typicode.com/users')
+    ret_dict = response1.json()
+    for dicts in ret_dict:
+        user_id = dicts["id"]
+        tasks = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+                user_id))
+        tasks = tasks.json()
+        for each in tasks:
+            each.pop("userId", None)
+            each.pop("id", None)
+            task_name = each.pop("title", None)
+            each["username"] = dicts["username"]
+            each["task"] = task_name
+        json_dict[str(user_id)] = tasks
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(json_dict, f)
